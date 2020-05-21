47266792b486345d497397f28f3b76721bc53bde
David Zaremsky <david.zaremsky@ge.com>
2020-05-21 01:08:56 -0400

    started refactor for game/index.js file

--- src/components/game/index.jsx
+++ src/components/game/index.jsx
@@ -1,122 +1,186 @@
-import React, { Fragment } from 'react';
-import Board from '../board/index.jsx';
-import { connect } from 'react-redux';
-import { setXIsNext, setStepNumber, setHistory } from '../../redux/game/actions';
-import { calculateWinner } from './helpers';
-import Button from '@material-ui/core/Button';
-import { Grid, Typography, List, Collapse, ListItem, ListItemText } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { ExpandLess, ExpandMore } from '@material-ui/icons'
-import styles from './styles';
+import React, { Fragment } from "react";
+import Board from "../board/index.jsx";
+import { connect, useSelector } from "react-redux";
+import {
+  setXIsNext,
+  setStepNumber,
+  setHistory,
+  setStatus,
+} from "../../redux/game/actions";
+import { calculateWinner } from "./helpers";
+import Button from "@material-ui/core/Button";
+import {
+  Grid,
+  Typography,
+  List,
+  Collapse,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
+import styles from "./styles";
+import { useEffect } from "react";
 
 const useStyles = makeStyles(styles);
 
-const Game = props => {
+const Game = (props) => {
+  // const { setStatus, status } = React.useState("Welcome to React Tac Toe!");
+  let statusBackup = React.useState();
+
   const classes = useStyles();
-  const handleClick = i => {
-    const { setXIsNext, xIsNext, setStepNumber, stepNumber, setHistory, history } = props;
+  const handleClick = (i) => {
+    const {
+      setXIsNext,
+      xIsNext,
+      setStepNumber,
+      stepNumber,
+      setHistory,
+      history,
+    } = props;
+    const tempHistory = history.slice(0, stepNumber + 1);
+    console.log(tempHistory);
+    const current = tempHistory[tempHistory.length - 1];
+    const squares = current.squares.slice();
+    console.log(history);
+
+    let tieGame;
+    // If Square is Full, Do Nothing
+    if (squares[i]) {
+      alert("Sorry Buster, That Square is Taken");
+      return;
+    }
+
+    // If Winner is Found
+    if (calculateWinner(squares)) {
+      return;
+    }
+
+    // If All Squares Are Full
+    if (tempHistory.length === 9) {
+      alert("It's a Tie!");
+      return;
+    }
+
+    turnHandler(i);
+  };
+
+  function turnHandler(turn, tieGame) {
+    const {
+      setXIsNext,
+      xIsNext,
+      setStepNumber,
+      stepNumber,
+      setHistory,
+      history,
+    } = props;
     const tempHistory = history.slice(0, stepNumber + 1);
     const current = tempHistory[tempHistory.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
+
     setXIsNext(!xIsNext);
     setStepNumber(stepNumber + 1);
-    squares[i] = xIsNext ? "X" : "O";
+    squares[turn] = xIsNext ? "X" : "O";
     setHistory(
       tempHistory.concat([
         {
-          squares: squares
-        }
+          squares: squares,
+        },
       ])
     );
   }
+
   const [open, setOpen] = React.useState(true);
-  const handleHistoryClick = () =>{
+  const handleHistoryClick = () => {
     setOpen(!open);
   };
-  const jumpTo = step => {
+  const jumpTo = (step) => {
     const { setStepNumber, setXIsNext } = props;
-    setStepNumber(step)
-    setXIsNext((step % 2) === 0)
-  }
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
   const { xIsNext, stepNumber, history } = props;
   const current = history[stepNumber];
   const winner = calculateWinner(current.squares);
   const moves = history.map((step, move) => {
-    const desc = move ?
-      'Go to move ' + move :
-      'Game Start';
-    return (<List key={move}>
-      <Button className={classes.button}
-        variant="outlined"
-        size="small"
-        fullWidth="true"
-        onClick={() => jumpTo(move)}>
-        {desc}
-      </Button>
-    </List>);
+    const desc = move ? "Go to move " + move : "Game Start";
+    return (
+      <List key={move}>
+        <Button
+          className={classes.button}
+          variant="outlined"
+          size="small"
+          fullWidth="true"
+          onClick={() => jumpTo(move)}
+        >
+          {desc}
+        </Button>
+      </List>
+    );
   });
-  let status;
+
   if (winner) {
-    status = winner + " Wins!";
+    statusBackup = winner + " Wins!";
+  } else {
+    statusBackup = (xIsNext ? "X" : "O") + "'s Turn";
   }
-  else {
-    status = (xIsNext ? "X" : "O") + "'s Turn";
-  }
+
+  // let status;
+
+  // status = useSelector((state) => state.status);
+
+  // useEffect(() => {});
+
+  // const { status } = useSelector((state) => state.status);
+
   return (
-  <Fragment>
-  <Grid container spacing={3} 
-  direction='column'
-  alignItems="center"
-  justify="flex-start"
-  >
-    <Grid item xs={12}>
-      <Typography variant={'h4'}>
-        {status}
-      </Typography>
-    </Grid>
-      <Grid item xs={12}>
-        <Board squares={current.squares} onClick={i => handleClick(i)} />
-      </Grid>
-      <ListItem button onClick={handleHistoryClick}>
-        {open ? <ExpandLess /> : <ExpandMore />}
-        <ListItemText align='center' primary="History" />
-        {open ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={!open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <Grid container 
-          direction='column'
-          alignItems="center"
-          justify="flex-start">
+    <Fragment>
+      <Grid
+        container
+        spacing={3}
+        direction="column"
+        alignItems="center"
+        justify="flex-start"
+      >
+        <Grid item xs={12}>
+          <Typography variant={"h4"}>{statusBackup}</Typography>
+          {/* <Typography variant={"h4"}>{status}</Typography> */}
+        </Grid>
+        <Grid item xs={12}>
+          <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+        </Grid>
+        <ListItem button onClick={handleHistoryClick}>
+          {open ? <ExpandLess /> : <ExpandMore />}
+          <ListItemText align="center" primary="History" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={!open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              justify="flex-start"
+            >
               {moves}
-          </Grid>
-        </List>
-      </Collapse>
-    </Grid>
-  </Fragment>
+            </Grid>
+          </List>
+        </Collapse>
+      </Grid>
+    </Fragment>
   );
-}
+};
 
-const mapStateToProps = (state) => (
-  {
-    xIsNext: state.game.xIsNext,
-    stepNumber: state.game.stepNumber,
-    history: state.game.history
-  }
-);
+const mapStateToProps = (state) => ({
+  xIsNext: state.game.xIsNext,
+  stepNumber: state.game.stepNumber,
+  history: state.game.history,
+});
 
-const mapDispatchToProps = (dispatch) => (
-  {
-    setXIsNext: action => dispatch(setXIsNext(action)),
-    setStepNumber: action => dispatch(setStepNumber(action)),
-    setHistory: action => dispatch(setHistory(action))
-  }
-);
+const mapDispatchToProps = (dispatch) => ({
+  setXIsNext: (action) => dispatch(setXIsNext(action)),
+  setStepNumber: (action) => dispatch(setStepNumber(action)),
+  setHistory: (action) => dispatch(setHistory(action)),
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Game)
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
