ffc4961bea1ae84b9497beb5edd0a7fc95d1eecc
David Zaremsky <david.zaremsky@ge.com>
2020-05-21 01:03:43 -0400

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { Provider } from 'react-redux';
-import theme from './theme';
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
-import './index.css';
-import Game from './components/game';
-import configureStore from './store/configureStore';
+import React from "react";
+import { render } from "react-dom";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { Provider } from "react-redux";
+import theme from "./theme";
+import "./index.css";
+import Game from "./components/game";
+import configureStore from "./store/configureStore";
 // ========================================
-
 
 render(
   <MuiThemeProvider theme={theme}>
-  <Provider store={configureStore()}>
-    <Game />
-  </Provider>
+    <Provider store={configureStore()}>
+      <Game />
+    </Provider>
   </MuiThemeProvider>,
-  document.getElementById("root"));
+  document.getElementById("root")
+);
