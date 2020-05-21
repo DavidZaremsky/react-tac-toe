3800fb63410ead2e49b4ddf28c5069a5fdd5f10c
David Zaremsky <david.zaremsky@ge.com>
2020-05-21 18:02:44 -0400

    removed isRequired from square component proptype

--- src/components/square/index.jsx
+++ src/components/square/index.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Square = props => {
-    const { onClick, value } = props;
-    return (
-        <button className="square" onClick={onClick}>
-            {value}
-        </button>
-    );
-}
-
-Square.propTypes = {
-    value: PropTypes.string.isRequired,
-    onClick: PropTypes.func
+const Square = (props) => {
+  const { onClick, value } = props;
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
-Square.defaultProps = {
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Square;
