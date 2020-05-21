15d13adee1e959a97b94e8b20a146b9b9dd2e44f
David Zaremsky <david.zaremsky@ge.com>
2020-05-21 01:08:25 -0400

    added reducer function for setStatus

--- src/redux/game/reducer.js
+++ src/redux/game/reducer.js
@@ -1,30 +1,35 @@
-import * as types from './types';
+import * as types from "./types";
 
 const INITIAL_STATE = {
   history: [
     {
-      squares: Array(9).fill(null)
-    }
+      squares: Array(9).fill(null),
+    },
   ],
   stepNumber: 0,
-  xIsNext: (Math.floor(Math.random() * 2) === 0)
-}
+  xIsNext: Math.floor(Math.random() * 2) === 0,
+  status: `Welcome To React Tac Toe! X Goes First`,
+};
 
 export default (state = INITIAL_STATE, action = {}) => {
   switch (action.type) {
     case types.SET_X_IS_NEXT:
       return Object.assign({}, state, {
-        xIsNext: action.value
-      })
+        xIsNext: action.value,
+      });
     case types.SET_STEP_NUMBER:
       return Object.assign({}, state, {
-        stepNumber: action.value
-      })
+        stepNumber: action.value,
+      });
     case types.SET_HISTORY:
       return Object.assign({}, state, {
-        history: action.history
+        history: action.history,
+      });
+    case types.SET_STATUS:
+      return Object.assign({}, state, {
+        status: action.value,
       });
     default:
       return state;
   }
-}
+};
