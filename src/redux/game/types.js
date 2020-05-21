c759fe65954a632a5260b1810ce553593a1a9019
David Zaremsky <david.zaremsky@ge.com>
2020-05-21 01:08:00 -0400

    added type for setStatus

--- src/redux/game/types.js
+++ src/redux/game/types.js
@@ -1,3 +1,4 @@
-export const SET_X_IS_NEXT = 'SET_X_IS_NEXT';
-export const SET_STEP_NUMBER = 'SET_STEP_NUMBER';
-export const SET_HISTORY = 'SET_HISTORY';
+export const SET_X_IS_NEXT = "SET_X_IS_NEXT";
+export const SET_STEP_NUMBER = "SET_STEP_NUMBER";
+export const SET_HISTORY = "SET_HISTORY";
+export const SET_STATUS = "SET_STATUS";
